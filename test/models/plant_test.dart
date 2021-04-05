ba823094f2a1ca0754560dc538ae6b040d4c4a47
Blaž Glogovčan <blaz.glogovcan@student.um.si>
2021-04-05 18:14:50 +0200

    Added more tests in model and care model

--- test/models/plant_test.dart
+++ test/models/plant_test.dart
@@ -4,7 +4,7 @@ import 'package:uuid/uuid.dart';
 
 void main() {
   group('PlantModel', () {
-    test('PlantModel with name Roža and empty notes', () {
+    test('Constructor with name Roža and empty notes', () {
       final plant = PlantModel(name: "Roza");
 
       expect(plant.name, "Roza");
@@ -13,13 +13,46 @@ void main() {
       expect(Uuid.isValidUUID(plant.id), true);
     });
 
-    //prazen name
+    test('Constructor with empty name and notes Notes', () {
+      final plant = PlantModel(notes: "Notes");
+
+      expect(plant.name, "");
+      expect(plant.notes, "Notes");
+    });
 
     test('Plant with id, name and notes', () {
       final plant = PlantModel(id: "1", name: "Kaktus", notes: "Ni obcutljiv");
 
       expect(plant.id, "1");
       expect(plant.notes, "Ni obcutljiv");
+    });
+
+    test('Clone', () {
+      final plant = PlantModel(name: "Kokos", notes: "Arrrrrr");
+      final clonePlant = plant.clone();
+
+      expect(clonePlant, isNot(plant));
+      expect(clonePlant.id, plant.id);
+      expect(clonePlant.name, plant.name);
+      expect(clonePlant.notes, plant.notes);
+    });
+
+    test('Get id', () {
+      final plant = PlantModel(id: "42069");
+
+      expect(plant.id, "42069");
+    });
+
+    test('Get name', () {
+      final plant = PlantModel(name: "banana");
+
+      expect(plant.name, "banana");
+    });
+
+    test('Get notes', () {
+      final plant = PlantModel(notes: "Cudni tegl");
+
+      expect(plant.notes, "Cudni tegl");
     });
 
     test('Set name', () {
@@ -45,7 +78,7 @@ void main() {
   });
 
   group('PlantCareModel', () {
-    test('PlantCareModel constructor', () {
+    test('Constructor', () {
       final plantCare =
           PlantCareModel(period: 14, last: DateTime.utc(1999, 8, 13));
 
@@ -53,14 +86,16 @@ void main() {
       expect(plantCare.last.month, 8);
     });
 
-    test('PlantCareModel update last', () {
-      final plantCare =
-          PlantCareModel(period: 1, last: DateTime.utc(2021, 1, 1));
+    test('Negative period in constructor, expecting exception', () {
+      expect(() => PlantCareModel(period: -20), throwsArgumentError);
+    });
 
-      plantCare.updateLast();
+    test('Last date in past, expecting exception', () {
+      final currentDate = DateTime.now();
+      final pastDate =
+          DateTime(currentDate.year, currentDate.month - 1, currentDate.day);
 
-      expect(plantCare.last.month, DateTime.now().month);
-      expect(plantCare.last.day, DateTime.now().day);
+      expect(() => PlantCareModel(last: pastDate), throwsArgumentError);
     });
 
     test('Clone', () {
@@ -72,15 +107,66 @@ void main() {
       expect(clone.last, plantCareModel.last);
     });
 
-    test('Negative period in constructor, expecting exception', () {
-      expect(() => PlantCareModel(period: -20), throwsArgumentError);
+    test('Update last', () {
+      final plantCare =
+          PlantCareModel(period: 1, last: DateTime.utc(2021, 1, 1));
+
+      plantCare.updateLast();
+
+      expect(plantCare.last.month, DateTime.now().month);
+      expect(plantCare.last.day, DateTime.now().day);
     });
 
-    //isto za set
+    test('Get period', () {
+      final plantCare = PlantCareModel(period: 5);
+
+      expect(plantCare.period, 5);
+    });
+
+    test('Get last', () {
+      final plantCare = PlantCareModel(last: DateTime(2021, 4, 5));
+
+      expect(plantCare.last, DateTime(2021, 4, 5));
+    });
+
+    test('Days till care, period null', () {
+      final care = PlantCareModel();
+      final days = care.daysTillCare;
+
+      expect(days, null);
+    });
+
+    test('Days till care, period 5, last now', () {
+      final care = PlantCareModel(period: 5);
+      final days = care.daysTillCare;
+
+      expect(days, 6); //danes +5 = 6
+    });
+
+    test('Days till care, negative, expecting exception', () {
+      //exception?
+      final care = PlantCareModel(period: -5);
+      final days = care.daysTillCare;
+
+      expect(days, isPositive);
+    });
+
+    test('Set period', () {
+      final care = PlantCareModel();
+      care.period = 21;
+
+      expect(care.period, 21);
+    });
+
+    test('Set period negative, expecting exception', () {
+      final care = PlantCareModel();
+
+      expect(() => care.period = -21, throwsArgumentError);
+    });
   });
 
   group('Json', () {
-    test('toJson', () {
+    test('PlantModel toJson', () {
       final plant = PlantModel(name: "Orhideja", notes: "Jakobova rozica");
       final json = {'name': 'Orhideja', 'notes': 'Jakobova rozica'};
       final actualJson = plant.toJson();
@@ -88,7 +174,8 @@ void main() {
       json.forEach((key, value) => expect(actualJson[key], value));
     });
 
-    test('fromJson', () {
+    test('PlantModel fromJson', () {
+      //nebi smelo failat, kontaktiraj Jakoba
       final plant =
           PlantModel.fromJson({'name': 'Orhideja', 'notes': 'Jakobova rozica'});
 
